fe1b2835f76369d9d8bc4f8b749a45ba917d3345
Patrice Calvé <Patrice.Calve@parl.gc.ca>
2021-03-10 22:07:21 -0500

    And now it's 3ta.mvc's turn to be fixed.

---
+++ src/3ta.mvc/Models/HomeWebApiViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3ta.mvc.Models
+{
+    public class HomeWebApiViewModel
+    {
+        public string WebApiUrl { get; set; }
+
+        public HomeWebApiViewModel()
+        {
+            Forecast = new List<WeatherForecast>();
+        }
+
+        public IList<WeatherForecast> Forecast { get; set; }
+
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+            public string Summary { get; set; }
+        }
+    }
+}
