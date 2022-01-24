fc83ce5768f7a69008cdddcf62b6da063a33f602
Patrice Calvé <Patrice.Calve@parl.gc.ca>
2022-01-24 07:32:36 -0500

    Add FebedbBackend user creation to db proj

---
+++ src/step3b.dockercompose/febedb.db/Security/FebedbBackend.sql
@@ -0,0 +1,17 @@
+﻿CREATE LOGIN [FebedbBackend]
+    WITH PASSWORD = N'Febedb@B4ckend'
+    , SID = 0x2A30987D5806E143900759FD32FFBAC1
+    , DEFAULT_LANGUAGE = [us_english];
+GO
+
+CREATE USER [FebedbBackend] FOR LOGIN [FebedbBackend];
+GO
+
+GRANT CONNECT TO [FebedbBackend]
+GO
+
+ALTER ROLE [db_datareader] ADD MEMBER [FebedbBackend]
+GO
+
+ALTER ROLE [db_datawriter] ADD MEMBER [FebedbBackend]
+GO
