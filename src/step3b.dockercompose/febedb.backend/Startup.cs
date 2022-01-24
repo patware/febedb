bb1320fa3af956f570ad6bf11353ef07a759e787
Patrice Calvé <Patrice.Calve@parl.gc.ca>
2022-01-24 07:38:19 -0500

    Entity Framework connStr pulled from config

--- src/step3b.dockercompose/febedb.backend/Startup.cs
+++ src/step3b.dockercompose/febedb.backend/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -27,9 +28,14 @@ namespace febedb.backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<Models.WeatherContext>(opt => 
+            //    opt.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=febedb.db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+
             services.AddDbContext<Models.WeatherContext>(opt => 
-                opt.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=febedb.db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+                opt.UseSqlServer(Configuration.GetConnectionString("febedbbackendContext")));
+
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "febedb.backend", Version = "v1" });
@@ -40,13 +46,14 @@ namespace febedb.backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "febedb.backend v1"));
             }
-
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
